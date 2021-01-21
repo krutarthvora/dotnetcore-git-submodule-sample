b074fa4775f64a8d14f437e4d161725f59bc1421
Krutarth <krutarth.d.vora@gmail.com>
2021-01-21 09:47:49 +0530

    initial commit

---
+++ dotnetcore-git-submodule-sample/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace dotnetcore_git_submodule_sample
+{
+  public class JsonHelper
+  {
+    public static string Serialize(object value)
+    {
+      string retu = "";
+      if (value != null)
+      {
+        JsonSerializerSettings settings = new()
+        {
+          TypeNameHandling = TypeNameHandling.Auto
+        };
+        retu = JsonConvert.SerializeObject(value, settings);
+      }
+      return retu;
+    }
+  }
+}

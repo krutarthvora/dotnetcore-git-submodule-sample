e4d7a0a3248ad936639627b70f2291e6583ef14a
Krutarth <krutarth.d.vora@gmail.com>
2021-01-26 11:16:37 +0530

    json helper additional methods

--- dotnetcore-git-submodule-sample/JsonHelper.cs
+++ dotnetcore-git-submodule-sample/JsonHelper.cs
@@ -1,30 +1,53 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace dotnetcore_git_submodule_sample
 {
-  public class JsonHelper
-  {
-    public static string Serialize(object value)
-    {
-      string retu = "";
-      if (value != null)
-      {
-        JsonSerializerSettings settings = new()
-        {
-          TypeNameHandling = TypeNameHandling.Auto
-        };
-        retu = JsonConvert.SerializeObject(value, settings);
-      }
-      return retu;
-    }
-    public static string Serialize4Print(object value)
-    {
-      string retu = "";
-      if (value != null)
-      {
-        retu = JsonConvert.SerializeObject(value, Formatting.Indented);
-      }
-      return retu;
-    }
-  }
+	public class JSONHelper
+	{
+		public static string Serialize(object value)
+		{
+			string retu = "";
+			if (value != null)
+			{
+				JsonSerializerSettings settings = new()
+				{
+					TypeNameHandling = TypeNameHandling.Auto
+				};
+				retu = JsonConvert.SerializeObject(value, settings);
+			}
+			return retu;
+		}
+		public static string Serialize4Print(object value)
+		{
+			string retu = "";
+			if (value != null)
+			{
+				retu = JsonConvert.SerializeObject(value, Formatting.Indented);
+			}
+			return retu;
+		}
+		public static T Deserialize<T>(string jsonValue)
+		{
+			JsonSerializerSettings settings = new()
+			{
+				TypeNameHandling = TypeNameHandling.Auto
+			};
+			T retu = JsonConvert.DeserializeObject<T>(jsonValue);
+			return retu;
+		}
+		public static string SerializeCamelCase(object value)
+		{
+			string retu = "";
+			if (value != null)
+			{
+				JsonSerializerSettings settings = new()
+				{
+					ContractResolver = new CamelCasePropertyNamesContractResolver()
+				};
+				retu = JsonConvert.SerializeObject(value, settings);
+			}
+			return retu;
+		}
+	}
 }
